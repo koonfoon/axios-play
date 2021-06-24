899193b53911daced9b5009888de1094090e0575
koonfoon <62646816+koonfoon@users.noreply.github.com>
2021-06-24 11:40:09 +0000

    cropping size to poster ratio

--- imageSize.js
+++ imageSize.js
@@ -1,8 +1,26 @@
 const sizeOf = require('image-size');
 const Path = require('path')
+const sharp = require('sharp');
 
 const imagePath = Path.resolve(__dirname, "downloaded", "ssis071pl.jpg");
 
 const dimensions = sizeOf(imagePath);
 
 console.log(dimensions.width, dimensions.height);
+
+const sizeToCrop = {
+  width: 378,
+  height: 538,
+  left: 422,
+  top: 0
+};
+
+const croppedFileNamePath = Path.resolve(__dirname, "downloaded", "poster.jpg");
+
+sharp(imagePath).extract(sizeToCrop).toFile(croppedFileNamePath)
+  .then(function(new_file_info){
+    console.log("Image cropped and saved");
+  })
+  .catch(function(error) {
+    console.log("An error occured");
+  })
